65443b471143469ca2b63b55d9372a82d00dade0
C:/Program Files/Git/Fathahillah <alethoriq1804@gmail.com>
2026-09-24 21:36:41 +0700

    create api project users

---
+++ bulid.sql
@@ -0,0 +1,5 @@
+-- show databases
+
+-- create database users_restfull_api_typescript_for_learning
+
+use users_restfull_api_typescript_for_learning
---
+++ lib/prisma.ts
@@ -0,0 +1,22 @@
+import "dotenv/config";
+import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+import { PrismaClient } from "../generated/prisma/client";
+
+function requireEnv(name: string): string {
+  const value = process.env[name];
+
+  if (!value) {
+    throw new Error(`Environment variable ${name} belum diatur`);
+  }
+
+  return value;
+}
+
+const adapter = new PrismaMariaDb({
+  host: requireEnv("DATABASE_HOST"),
+  user: requireEnv("DATABASE_USER"),
+  password: requireEnv("DATABASE_PASSWORD"),
+  database: requireEnv("DATABASE_NAME"),
+  connectionLimit: 5,
+});
+const prisma = new PrismaClient({ adapter });
