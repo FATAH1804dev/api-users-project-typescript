9c6b2c2edfce4fde9426e0dcbc5347798bb856a7
C:/Program Files/Git/Fathahillah <alethoriq1804@gmail.com>
2026-09-26 17:17:27 +0700

    create application

---
+++ application/database.ts
@@ -0,0 +1,29 @@
+import { adapter } from "../lib/prisma";
+import { PrismaClient } from "../generated/prisma/client";
+import { logger } from "./logging";
+
+export const prisma = new PrismaClient({
+  adapter,
+  log: [
+    { emit: "event", level: "query" },
+    { emit: "event", level: "info" },
+    { emit: "event", level: "warn" },
+    { emit: "event", level: "error" },
+  ],
+});
+
+prisma.$on("error", (e) => {
+  logger.error(e);
+});
+
+prisma.$on("warn", (e) => {
+  logger.warn(e);
+});
+
+prisma.$on("info", (e) => {
+  logger.info(e);
+});
+
+prisma.$on("query", (e) => {
+  logger.info(e);
+});
